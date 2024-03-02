f02e5f289bde45b265d491b09cf0c6c33477fa43
Thibault <76521322+thibault0712@users.noreply.github.com>
2024-03-02 12:01:19 +0100

    Update firebase.js

--- src/data/firebase.js
+++ src/data/firebase.js
@@ -7,13 +7,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDDZFtnvpl_-4LLeIqjnkQ3tLtc5c9CeNo",
-  authDomain: "novasuite.firebaseapp.com",
-  projectId: "novasuite",
-  storageBucket: "novasuite.appspot.com",
-  messagingSenderId: "1655999460",
-  appId: "1:1655999460:web:9d261fac5a60de3f5fd5f1",
-  measurementId: "G-9N9JRTY0RZ"
+  apiKey: "x",
+  authDomain: "x",
+  projectId: "x",
+  storageBucket: "x",
+  messagingSenderId: "x",
+  appId: "x",
+  measurementId: "x"
 };
 
 // Initialize Firebase
